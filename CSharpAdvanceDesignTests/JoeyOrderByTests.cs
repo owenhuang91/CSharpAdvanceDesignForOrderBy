f5c6e05e41a851885de7d38091be2a3b29bc103e
Owen <owen2862@live.com>
2019-03-17 12:17:34 +0800

    Add project files.

---
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ExpectedObjects;
+using Lab.Entities;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace CSharpAdvanceDesignTests
+{
+    [TestFixture]
+    [Ignore("not yet")]
+    public class JoeyOrderByTests
+    {
+        [Test]
+        public void orderBy_lastName()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Wang"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyOrderByLastName(employees);
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Wang"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        
+        private IEnumerable<Employee> JoeyOrderByLastName(IEnumerable<Employee> employees)
+        {
+            //bubble sort
+            var stringComparer = StringComparer.Create(CultureInfo.CurrentCulture, true);
+            var elements = employees.ToList();
+            while (elements.Any())
+            {
+                var minElement = elements[0];
+                var index = 0;
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    if (stringComparer.Compare(elements[i].LastName, minElement.LastName) < 0)
+                    {
+                        minElement = elements[i];
+                        index = i;
+                    }
+                }
+
+                elements.RemoveAt(index);
+                yield return minElement;
+            }
+        }
+    }
+}
